d505847e43041cd89feddb03fd387b08eb79b299
Dylan Nelson <dylanonelson@gmail.com>
2021-07-31 18:46:53 -0400

    Consolidate into html

--- config.js
+++
@@ -1,8 +0,0 @@
-const nconf = require('nconf');
-
-nconf
-  .argv()
-  .env()
-  .file({ file: './config.json' });
-
-module.exports = nconf;
--- index.js
+++
@@ -1,8 +0,0 @@
-require('babel-register')({
-  presets: ['env', 'react']
-});
-
-const app = require('./server');
-const config = require('./config');
-
-app.listen(process.env.PORT || config.get('PORT'));
